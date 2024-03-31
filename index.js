7bc47aa4614c15c94c8f9c8609a818abb61c8f69
G. Akbar <ghulamakbarabbbro110@gmail.com>
2024-03-31 06:36:33 +0500

    after publishing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// #! /usr/bin/env node
 import inquirer from "inquirer";
 let balance = 100000;
-let pin = 1212;
+let pin = 1234;
 const atm = await inquirer.prompt([
-    { name: "Name", message: "Pease Write your name", type: "string" },
+    { name: "Name", message: "Please Write your name", type: "string" },
     { name: "Q1", type: "number", message: "Please enter your pin" },
 ]);
 if (atm.Q1 === pin) {
@@ -10,7 +11,7 @@ if (atm.Q1 === pin) {
     const question = await inquirer.prompt([{
             name: "Q2",
             type: "list",
-            message: "Please slect the option",
+            message: "Please select the option",
             choices: ["Withdraw", "Check Balance"]
         }
     ]);
@@ -36,8 +37,43 @@ if (atm.Q1 === pin) {
     else if (question.Q2 === "Check Balance") {
         console.log(balance);
     }
-    else {
-        console.log("Wrong pin");
-        console.log(`Dear ${atm.Name} please enter the wright pin code`);
+}
+else {
+    console.log("Wrong pin code");
+    console.log(`Dear ${atm.Name} please enter the right pin code`);
+    const atm1 = await inquirer.prompt([
+        { name: "Q1", type: "number", message: "Please enter your pin" },
+    ]);
+    if (atm1.Q1 === pin) {
+        console.log("Correct pin code");
+        const question = await inquirer.prompt([{
+                name: "Q2",
+                type: "list",
+                message: "Please select the option",
+                choices: ["Withdraw", "Check Balance"]
+            }
+        ]);
+        if (question.Q2 === "Withdraw") {
+            const withdrawAmount = await inquirer.prompt([{
+                    name: "WA", type: "number", message: "Enter your amount"
+                }]);
+            if (withdrawAmount.WA > balance) {
+                console.log("Sorry! Sir you can't wthrow " + withdrawAmount.WA + " " + "from" + " " + balance);
+                const otherwithdrawAmount = await inquirer.prompt([{
+                        name: "WA", type: "number", message: "Enter your amount"
+                    }]);
+                if (otherwithdrawAmount.WA < balance) {
+                    let BalanceDeduction = balance - otherwithdrawAmount.WA;
+                    console.log(`Dear ${atm.Name} Your remaining balance is = ${BalanceDeduction}`);
+                }
+            }
+            else {
+                let BalanceDeduction = balance - withdrawAmount.WA;
+                console.log(`Dear ${atm.Name} Your remaining balance is = ${BalanceDeduction}`);
+            }
+        }
+        else if (question.Q2 === "Check Balance") {
+            console.log(balance);
+        }
     }
 }
