bdb917ad17547f07594b8c8beaeec334ee756e25
G. Akbar <ghulamakbarabbbro110@gmail.com>
2024-03-31 18:53:14 +0500

    compete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// #! /usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let balance = 100000;
 let pin = 1234;
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-// #! /usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer"
 let balance = 100000
 let pin:number = 1234
